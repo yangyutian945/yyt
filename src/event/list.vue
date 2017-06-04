5efddd32b68a5b439b8bcb97bc212232e2ae5339
yangyutian945 <2921189094@qq.com>
2017-06-05 00:29:58 +0800

    Add files via upload

---
+++ src/event/list.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <el-tabs v-model="activeTab">
+            <el-tab-pane label="进行中" name="tab1">
+                <p>线上积分送酒</p>
+            </el-tab-pane>
+            <el-tab-pane label="未开始" name="tab2">
+                <p>未开始</p>
+            </el-tab-pane>
+            <el-tab-pane label="已结束" name="tab3">
+                <p>已结束</p>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                activeTab: 'tab1',
+            };
+        },
+    };
+</script>
+<style>
+
+</style>
---
+++ src/home/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>后台管理系统</h1>
+        <h2>测试中...</h2>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+            };
+        }
+    };
+</script>
+<style>
+
+</style>
